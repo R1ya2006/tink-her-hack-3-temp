6fba3902890a18a6d61d1b2a1e6cc372ae4cec22
R1ya2006 <riyasinny2006@gmail.com>
2025-02-09 10:17:15 +0530

    Add files via upload

---
+++ SlideshowViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.tranquil_zone.ui.slideshow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SlideshowViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
